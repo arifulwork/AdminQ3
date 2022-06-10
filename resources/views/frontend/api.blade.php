06affb6faa4f443bdea13efb7f114a42061ba5fd
Md Ariful Islam <m.ariful@graduate.utm.my>
2022-06-11 03:34:59 +0600

    middleware setup

--- resources/views/frontend/api.blade.php
+++ resources/views/frontend/api.blade.php
@@ -29,26 +29,19 @@ Route::get('/', function () {
 });
 */
 
+
+
+
+
+
+Route::get('/reg', [AddController::class, 'indexReg']); // Register Login
 Route::post('/checkreg', [AddController::class, 'adminsave']); // Register Login
+
+Route::get('/', [AddController::class, 'indexLogin']); // Login
+
 Route::post('/checklogin', [AddController::class, 'check']); // Login
-Route::post('/add-class', [addNewClassController::class, 'store']);
-Route::put('/updatesubject/{subject_id}', [addNewClassController::class, 'subjectupdate']); // Update Class Data
-Route::post('/add-course', [AddCourse::class, 'coursess']);
-Route::put('/updateCourse/{course_id}', [AddCourse::class, 'courseupdate']); // Update Course Data
-Route::post('/class-reservation', [addStudent::class, 'addThem']); // StudentReservationSystem
 
-Route::post('/assignCourse', [addStudent::class,'kitter'])->name('available.check');; // Email
-
-Route::put('/updateInfo/{student_id}', [addStudent::class, 'studentInfoUpdate']); // Update Course Data
-
-/*Middleware */
-
-Route::group(['middleware'=>['AuthCheck']], function(){
-
-    Route::get('/reg', [AddController::class, 'indexReg']); // Register Login
-    Route::get('/', [AddController::class, 'indexLogin']); // Login
-
-    Route::get('/admindashboard', [AddController::class, 'index']); // Class Add Controller
+Route::get('/admindashboard', [AddController::class, 'index']); // Class Add Controller
 
 Route::get('/classschedule/{course_id}', [classScheduleAdminView::class, 'classShedule']);
 
@@ -56,22 +49,25 @@ Route::get('/admindashboard', [adminCourseView::class, 'courseShedule']);
 Route::get('/searchclass', [adminCourseView::class,'searchCourse']);  // SearchBox Class
 
 Route::get('/add-class', [addNewClassController::class, 'addClass']);
-
+Route::post('/add-class', [addNewClassController::class, 'store']);
 Route::get('edit-subject/{subject_id}', [addNewClassController::class, 'editSubject']);  // Edit Course Data
-
+Route::put('/updatesubject/{subject_id}', [addNewClassController::class, 'subjectupdate']); // Update Class Data
 Route::get('/Deletesubject/{subject_id}', [addNewClassController::class, 'deleteClass']); // Delete Class Data
 
 
 Route::get('/add-course', [AddCourse::class, 'addcouses']);
-
+Route::post('/add-course', [AddCourse::class, 'coursess']);
 Route::get('/searchcourse', [AddCourse::class,'search']);  // SearchBox Course
 
 Route::get('edit-course/{course_id}', [AddCourse::class, 'editCourse']);  // Edit Course Data
-
+Route::put('/updateCourse/{course_id}', [AddCourse::class, 'courseupdate']); // Update Course Data
 Route::get('/DeleteCourse/{course_id}', [AddCourse::class, 'deletecourse']); // Delete Course Data
 
 
+
 /* Student List */
+
+
 
 Route::get('/students-list', [StudentList::class, 'studList']);
 
@@ -85,11 +81,17 @@ Route::get('/searchStudents', [StudentList::class,'searchStudent']);  // SearchB
 /* Add Student */
 
 Route::get('/assignCourse', [addStudent::class, 'StudentAdd']); // Add Student from Admin
+
+Route::post('/class-reservation', [addStudent::class, 'addThem']); // StudentReservationSystem
+
+Route::post('/assignCourse', [addStudent::class,'kitter'])->name('available.check');; // Email
+
 Route::get('/assignCourse', [addStudent::class,'redirectSubject']); // View Select
 
 
 /*Edit and Update Student */
 
 Route::get('/edit-info/{student_id}', [addStudent::class, 'editStudentInfo']);  // Edit Course Data
+Route::put('/updateInfo/{student_id}', [addStudent::class, 'studentInfoUpdate']); // Update Course Data
 Route::get('/DeleteStudent/{student_id}', [addStudent::class, 'deleteStudentInfo']); // Delete Student Data
-});
+
